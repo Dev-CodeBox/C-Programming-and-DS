683478a9c1ff290b63365b8d440a47e0adadb263
Dev Raj Singh <dev.maildrop@gmail.com>
2025-08-26 20:32:42 +0530

    Operators & Expressions

--- Basics-Unit-1/main.c
+++ Basics-Unit-1/main.c
@@ -7,5 +7,9 @@ int main()
     // Identifier & Keywords
     int myVariable = 5;
     printf("Value of myVariable: %d\n", myVariable);
+
+    // Operators & Expressions
+    int sum = myVariable + 10;
+    printf("Value of sum: %d\n", sum);
     return 0;
 }
